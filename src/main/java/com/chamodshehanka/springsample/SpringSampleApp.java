0ce56642a2ef0ec594c4bd0a2c85b601fcf5f2e2
Shehanka <hcsperera@outlook.com>
2019-03-06 15:45:23 +0530

    Annotation added

---
+++ src/main/java/com/chamodshehanka/springsample/SpringSampleApp.java
@@ -0,0 +1,15 @@
+package com.chamodshehanka.springsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chamodshehanka on 3/6/2019
+ * @project spring-sample
+ **/
+@SpringBootApplication
+public class SpringSampleApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSampleApp.class, args);
+    }
+}
